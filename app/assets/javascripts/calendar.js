2daa69a3c47137dd3711f965e356259f7f812352
Yuta Yasugahira <hourou_freak@yahoo.co.jp>
2016-07-19 14:21:24 +0000

    Modify URL

--- app/assets/javascripts/calendar.js
+++ app/assets/javascripts/calendar.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
     var select = function(start, end, allDay) {
 //        var title = window.prompt("title");
-        var title = window.open("https://subaco-yasugahira0810.c9users.io/events/new", '_blank',
+        var title = window.open("https://subaco2-yasugahira0810.c9users.io/events/new", '_blank',
         'toolbar=no, location=no, directories=no, status=no, menubar=no, scrollbars=yes, resizable=yes')
         var data = {event: {title: title,
                             start: start.format(),
