14cb676b030ad5b0889156dfe10a7e375700e394
Yuta Yasugahira <hourou_freak@yahoo.co.jp>
2016-09-11 14:01:51 +0000

    Finalize messages preparing for v0.0.1

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,14 @@ User.create!(name: "安ヶ平　雄太",
              activated: true,
              activated_at: Time.zone.now)
 
+User.create!(name: "俺ヶ平　俺太",
+             email: "oregahira@sample.co.jp",
+             password:              "password",
+             password_confirmation: "password",
+             admin: false,
+             activated: true,
+             activated_at: Time.zone.now)
+
 99.times do |n|
   name  = Faker::Name.name
   email = "example-#{n+1}@railstutorial.org"
